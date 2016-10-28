df6d8811661b17120a6cbad7521b4b4ae8f5adcd
Anderson <rerafyaw@github.com>
2016-10-27 22:48:32 -0400

    Updated window.py: supports Serial communication

--- window.py
+++ window.py
@@ -1,14 +1,18 @@
 from Tkinter import *
+import serial
 
 #Somehow getting a direction input from Arduino, ME stuff thing
 # Blah blah
 direction = 'UP'
 
+# Set up serial interface, at 9600 bps
+ser = serial.Serial('/dev/ttyUSB0',9600)
+
 
 class direction(Frame):
     def __init__(self, master):
         Frame.__init__(self, master, bg = 'white')
-        self.dirText = Label(self, font=('Helvetica',400), fg="black", bg="white",text='UP')
+        self.dirText = Label(self, font=('Helvetica',100), fg="black", bg="white",text='UP')
         self.dirText.pack(side = TOP, anchor = E)
 
 
@@ -26,6 +30,7 @@ class fullWindow:
 if __name__ == '__main__':
     sung = fullWindow()
     while True:
+        read_serial = str(ser.readline())
         sung.rootWin.update_idletasks()
         sung.rootWin.update()
-        sung.direction.dirText.config(text = 'DOWN')
+        sung.direction.dirText.config(text = read_serial)
