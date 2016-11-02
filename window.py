ecfb6e2baf692d5840e34a1db88b9c29415be12e
Sung Park <sungwoo.park@students.olin.edu>
2016-11-02 02:01:15 -0400

    clock and weather implemented -- very ugly

--- window.py
+++ window.py
@@ -2,13 +2,15 @@ from Tkinter import *
 import serial
 import time
 import requests
+from PIL import ImageTk, Image
+from cStringIO import StringIO
+from io import BytesIO
 
 #Somehow getting a direction input from Arduino, ME stuff thing
 direction = 'UP'
 
 # Set up serial interface, at 9600 bps
 ##ser = serial.Serial('/dev/ttyUSB0',9600)
-
 
 class direction(Frame):
     def __init__(self, master):
@@ -25,41 +27,68 @@ class clock(Frame):
         currentTime = time.strftime('%I: %M %p')
         return currentTime
 
-
 class weather(Frame):
     def __init__(self, master):
-        Frame.__init__(self, master, bg = 'red')
-        self.weatherText = Label(self, font=('Helvetica',100), fg="black", bg="white",text='WEATHER')
-        self.weatherText.pack(side = TOP, anchor = E)
+        Frame.__init__(self, master, bg = 'blue')
+        #CREATE TEMP LABEL
+        self.weatherTemp = Label(self, font=('Helvetica',100), fg="black", bg="white",text='TEMP')
+        self.weatherTemp.pack(side = TOP, anchor = E)
+        #CREATE IMAGE LABEL
+        pic = Image.open('weather_placeholder.png')
+        self.weatherImage = ImageTk.PhotoImage(pic)
+        self.weatherPane = Label(self, image = self.weatherImage)
+        self.weatherPane.pack(side = BOTTOM, anchor = E)
+        #CREATE DESCRIPTION LABEL
+        self.weatherDescription = Label(self, font=('Helvetica',40), fg="black", bg="white",text='Description')
+        self.weatherDescription.pack(side = TOP, anchor = E)
+        #CREATE REQUESTS OBJECT
         self.request = requests
-    def getWeather(self): #TODO: change to temp -- getTemp, tempText, etc
+    def getWeather(self):
         self.cityName = 'Needham'
         APPID = '11dee608927c6bc5f293a9ee96e5ad91'
         weatherURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + self.cityName + '&APPID=' + APPID 
         weatherData = self.request.get(weatherURL)
         weatherJSON = weatherData.json()
         weatherTemp = weatherJSON['main']['temp'] #example: 282.56 -- in Kelvin
-##        weatherSky = weatherJSON['weather']['main'] #example: 'Clear', 'Rain', 'Snow'
-        #weatherIconID = weatherJSON['weather']['icon'] -- returns id of icon (example: 'Old'?)
-        return weatherTemp
-    
+        weatherFahr = int(round((weatherTemp - 273)*9/5 + 32)) #convert to degrees Fahrenheit
+        weatherSky = weatherJSON['weather'][0]['description'] #example: 'Clear', 'Rain', 'Snow'
+        weatherIconID = weatherJSON['weather'][0]['icon'] #returns id of icon (example: 'Old'?)
+        return weatherFahr, weatherSky, weatherIconID
+    def updateWeather(self):
+            processedData = self.getWeather()
+            #UPDATE TEMPERATURE STRING
+            currentTemp = str(processedData[0]) + ' F'
+            self.weatherTemp.config(text = currentTemp)
+            #UPDATE ICON PICTURE
+            iconURL = 'http://openweathermap.org/img/w/' + processedData[2] + '.png'
+            iconData = self.request.get(iconURL)
+            iconPicture = ImageTk.PhotoImage(Image.open(BytesIO(iconData.content)))
+            self.weatherPane.config(image = iconPicture)
+            self.weatherPane.image = iconPicture
+            #UPDATE DESCRIPTION
+            currentDesc = str(processedData[1])
+            self.weatherDescription.config(text = currentDesc)
 
 class fullWindow:
     def __init__(self):
         self.rootWin = Tk()
         self.rootWin.configure(background='black')
         self.rootWin.attributes("-fullscreen", True)
+        self.time = time.time() - 5*60; #this makes the loop below update weather right away
 
+        #DIRECTION
         self.leftFrame = Frame(self.rootWin, background = 'white') #create frame for direction label
         self.leftFrame.pack(expand=False, fill = 'both', side = LEFT) #put frame against LEFT side, fill frame in x and y directions
         self.direction = direction(self.leftFrame) #create direction object in leftFrame
         self.direction.pack(side = TOP) #put direction object in frame (against LEFT side)
 
+        #CLOCK
         self.rightFrame = Frame(self.rootWin, background = 'white') #create frame for clock label
         self.rightFrame.pack(expand=False, fill = 'both', side = LEFT) #put frame against RIGHT side, fill frame in x and y directions
         self.clock = clock(self.rightFrame) #create clock object in rightFrame
         self.clock.pack(side = BOTTOM ) #put clock object in frame (against RIGHT side)                                
 
+        #WEATHER
         self.rightFrame = Frame(self.rootWin, background = 'white') #create frame for clock label
         self.rightFrame.pack(expand=False, fill = 'both', side = RIGHT) #put frame against RIGHT side, fill frame in x and y directions
         self.weather = weather(self.rightFrame) #create clock object in rightFrame
@@ -71,8 +100,12 @@ if __name__ == '__main__':
 ##        read_serial = str(ser.readline())
         sung.rootWin.update_idletasks()
         sung.rootWin.update()
-        sung.direction.dirText.config(text = 'bob') #read_serial)
+        #DIRECTION UPDATE
+##        sung.direction.dirText.config(text = read_serial)
+        #CLOCK UPDATE
         currentTime = sung.clock.getTime()
         sung.clock.clockText.config(text = currentTime)
-        currentTemp = str(sung.weather.getWeather())
-        sung.weather.weatherText.config(text = currentTemp)
+        #WEATHER UPDATE
+        if time.time() - sung.time > 5*60: #if it's been 5 minutes, check weather again
+            sung.weather.updateWeather()
+            sung.time = time.time()
