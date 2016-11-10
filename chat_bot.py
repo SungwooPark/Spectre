32024b500af48dae3f58432b4a5236143e651465
Sung Park <sungwoo.park@students.olin.edu>
2016-11-09 19:56:18 -0500

    Add chatbot response function and make chatbot interface class

--- chat_bot.py
+++ chat_bot.py
@@ -2,25 +2,34 @@ from chatterbot import ChatBot
 import speech_rec
 import subprocess
 
-def say_output(s):
-    '''
-    Say an argument string using bash say command
-    '''
-    commands = ['say', s]
-    process = subprocess.Popen(commands) 
-    output, error = process.communicate()    
+class ChatBotInterface:
 
-chatbot = ChatBot(
-    'Spectre',
-    trainer='chatterbot.trainers.ChatterBotCorpusTrainer'
-)
+    def __init__(self):
 
-chatbot.train("chatterbot.corpus.english")
+        self.chatbot = ChatBot(
+            'Aaron Hoover'
+            trainer='chatterbot.trainers.ChatterBotCorpusTrainer'
+        )
+
+        self.chatbot.train("chatterbot.corpus.english")
+
+    def say_output(s):
+        '''
+        Say an argument string using bash say command
+        '''
+        commands = ['say', s]
+        process = subprocess.Popen(commands) 
+        output, error = process.communicate()
+
+    def chatbot_response(s):
+        '''
+        Return a response of a chatbot
+        '''
+        return str(chatbot.get_response(prompt))
 
 if __name__ ==  '__main__':
-    print 'Spectre: Hi, My name is Spectre.'
+    print 'Spectre: Hi, My name is Spectre'
     say_output('Hi, my name is Spectre')
     prompt = speech_rec.get_microphone_output()
     print 'You: ', prompt
     say_output(str(chatbot.get_response(prompt)))
-
