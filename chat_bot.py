ee66b811f542550ed1bf7928cad67d0df51e0cd6
Sung Park <sungwoo.park@students.olin.edu>
2016-11-10 16:56:49 -0500

    Fix a number of syntax errors

--- chat_bot.py
+++ chat_bot.py
@@ -7,13 +7,13 @@ class ChatBotInterface:
     def __init__(self):
 
         self.chatbot = ChatBot(
-            'Aaron Hoover'
+            'Aaron Hoover',
             trainer='chatterbot.trainers.ChatterBotCorpusTrainer'
         )
 
         self.chatbot.train("chatterbot.corpus.english")
 
-    def say_output(s):
+    def say_output(self,s):
         '''
         Say an argument string using bash say command
         '''
@@ -21,15 +21,17 @@ class ChatBotInterface:
         process = subprocess.Popen(commands) 
         output, error = process.communicate()
 
-    def chatbot_response(s):
+    def chatbot_response(self,s):
         '''
         Return a response of a chatbot
         '''
-        return str(chatbot.get_response(prompt))
+        return str(self.chatbot.get_response(s))
 
 if __name__ ==  '__main__':
-    print 'Spectre: Hi, My name is Spectre'
-    say_output('Hi, my name is Spectre')
-    prompt = speech_rec.get_microphone_output()
-    print 'You: ', prompt
-    say_output(str(chatbot.get_response(prompt)))
+    cb = ChatBotInterface()
+    print cb.chatbot_response('hi, how are you doing')
+    #print 'Spectre: Hi, My name is Spectre'
+    #say_output('Hi, my name is Spectre')
+    #prompt = speech_rec.get_microphone_output()
+    #print 'You: ', prompt
+    #say_output(str(chatbot.get_response(prompt)))
